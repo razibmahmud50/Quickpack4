ead708fa2e00b353c48175e24f525592e16c1895
razibmahmud50 <razibmahmud50@gmail.com>
2019-04-21 12:26:21 +0600

    bootstrap 4.3 added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 (function ($) {
 	"use strict";
 
+    $('#idor, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
 
-    // jQuery methods go here...
-
-	
 }(jQuery));
